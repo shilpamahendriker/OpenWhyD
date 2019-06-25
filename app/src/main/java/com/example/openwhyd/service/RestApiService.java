1dedf2ab9172cff40104f7f62b8da2c95911c810
Shilpa Mahendriker <37084465+shilpamahendriker@users.noreply.github.com>
2019-06-25 18:04:26 -0400

    Update RestApiService.java

--- app/src/main/java/com/example/openwhyd/service/RestApiService.java
+++ app/src/main/java/com/example/openwhyd/service/RestApiService.java
@@ -11,6 +11,6 @@ public interface RestApiService {
     String BASE_URL = "https://openwhyd.org/";
 
     // endpoint to retrieve arraylist for loading in the recyclerview in main activity
-    @GET("hot?format=json")
+    @GET("hot?format=json&limit=100")
     Call<TrackWrapper> getTracks();
 }
